db79b94c19b1f093d991a4616880ba03d595869a
Christian Sabor <christiansabor@gmail.com>
2018-02-19 22:07:22 +0100

    Add implementation

---
+++ ExtractWebmVideo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace ExtractWebmVideo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: ExtractWebmVideo c:\\directory\\to\\import");
+                return;
+            }
+            try
+            {
+                var files = Directory.GetFiles(args[0]);
+                foreach (var file in files)
+                {
+                    ImortFile(file);
+                }
+            } 
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("The directory could not be found: " + args[0]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading directory: " + ex.Message);
+            }
+        }
+
+        private static void ImortFile(string file)
+        {
+            try
+            {
+                byte[] inputBytes = File.ReadAllBytes(file);
+                var start = FindStart(inputBytes);
+                if (start == -1)
+                {
+                    Console.WriteLine("No start found for file " + file + ", no video could be extracted");
+                }
+                else
+                {
+                    var fileNameOut = file + "-converted.webm";
+                    var target = File.OpenWrite(fileNameOut);
+                    target.Write(inputBytes, start, inputBytes.Length - start);
+                    target.Close();
+                    Console.WriteLine("Extracted video from " + file + " into " + fileNameOut);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error extracting video from " + file + ": " + ex.Message);
+            }
+        }
+
+        private static int FindStart(byte[] inputBytes)
+        {
+            for (var i = 0; i < inputBytes.Length - 3; i++)
+            {
+                if (inputBytes[i] == 0x1A
+                    && inputBytes[i + 1] == 0x45
+                    && inputBytes[i + 2] == 0xDF
+                    && inputBytes[i + 3] == 0xA3)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ ExtractWebmVideo/readme.md
@@ -0,0 +1,26 @@
+﻿ExtractWebmVideo
+================
+
+This small utility can extract videos from Just Dance 4 Autodance files by searching for the video header and moving the content that follows it to an individual file.
+
+## Usage
+
+```
+ExtractWebmVideo c:\directory\to\search
+```
+
+## Import for Xbox 360 Autodance files
+
+To use this utility to extract your videos from saved autodances on Xbox 360 you first have to connect the Xbox-hard-drive to your computer as described here: https://www.makeuseof.com/tag/how-to-connect-your-xbox-360-hard-drive-to-your-pc/
+and then download the Just Dance content to your local HD using https://fatxplorer.eaton-works.com/ 
+
+## Playback errors
+
+In my experience, the resulting videos have some encoding errors on the container level, in case the file can't be played because of the errors you can fix the container by repackaging the using ngvtoolnix https://mkvtoolnix.download/downloads.html#windows 
+
+The command-line for repackaging all files in a directory: 
+```
+FOR %I in (c:\justdance\output\*.webm) DO mkvmerge -o %~dI%~pIfixed\%~nI%~xI --webm %~fI
+```
+
+To analyze your video files you can use ngvtoolnix (linked above) or https://www.videohelp.com/software/MediaInfo
